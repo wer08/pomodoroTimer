cdaa68e347bf87e799c1c677013fe8d5cea68a2a
wer08 <wz08061992@gmail.com>
2023-01-02 12:24:41 +0100

    Added changind time

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View, Vibration } from 'react-native';
+import { Button, StyleSheet, Text, View, Vibration, TextInput } from 'react-native';
 
 let timer;
 
 export default function App() {
-  const [time, setTime] = useState(1500000);
+  const [workTime, setWorkTime] = useState('25:00');
+  const [breakTime, setBreakTime] = useState('5:00');
+  const [time, setTime] = useState(workTime);
   const [work,setWork] = useState(true);
+
 
   function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
@@ -17,19 +20,29 @@ export default function App() {
       minutes + ":" + (seconds < 10 ? "0" : "") + seconds
     );
   }
+
+  const MinutestoMillseconds = (minutesAndSeconds) =>{
+        minutesAndSeconds = minutesAndSeconds.split(":");
+        let minutes = minutesAndSeconds[0];
+        let seconds = minutesAndSeconds[1];
+        let finalTime = (((parseInt(minutes)*60)+parseInt(seconds)) * 1000);
+        return finalTime;
+  }
+
   useEffect(()=>{
     console.log(work)
     if(work)
     {
-      setTime(1500000);
+      setTime(MinutestoMillseconds(workTime));
     }
     else{
-      setTime(300000);
+      setTime(MinutestoMillseconds(breakTime));
     }
   },[work])
 
   useEffect(()=>{
     if(time === 0){
+      console.log(time)
       setWork(!work);
       Vibration.vibrate(1000);
     }
@@ -58,17 +71,43 @@ export default function App() {
   const handleRestart = () => {
     if(work)
     {
-      setTime(1500000)
+      setTime(MinutestoMillseconds(workTime))
     }
     else
     {
-      setTime(300000)
+      setTime(MinutestoMillseconds(breakTime))
     }
     Vibration.vibrate(600);
   }
+  const timeReg = /^([0-9]{0,3}:[0-5]{0,1}[0,9]{0,1})$/;
+  const handleWork = (work) => {
+    if(timeReg.test(work))
+    {
+      setWorkTime(work);
+    }
+
+  };
+  const handleBreak = (breakT) => {
+    if(timeReg.test(breakT))
+    {
+      setBreakTime(breakT);
+    }
+
+  };
+
+  
+
+
 
   return (
     <View style={styles.container}>
+        <Text>Enter work time(default = 25 min)</Text>
+        <TextInput keyboardType='number-pad' style={styles.input} onChangeText={handleWork} value={workTime} />
+        <Text>Enter break time(default = 5 min)</Text>
+        <TextInput keyboardType='number-pad' style={styles.input} onChangeText={handleBreak} value={breakTime}/>
+        <Button title="Set New times" onPress={()=>setTime(MinutestoMillseconds(workTime))} />
+    
+
       <Text style={styles.time}>{millisToMinutesAndSeconds(time)}</Text>
       <View style={styles.button}>
         <Button title='Start' onPress={handleStart}/>
@@ -95,5 +134,11 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 20,
+  },
+  input:{
+    borderWidth: 2,
+    borderColor: 'black',
+    marginBottom: 20,
+    padding: 5
   }
 });
